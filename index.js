afa1852cba36acaea10c6b172209a24c4334fd91
austin <austin@austin-laptop.(none)>
2011-08-05 13:32:50 -0500

    Changed slice.call(arguments),unshift with indetifier to [].concat([this],slice.call)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 (function(){
-
 var prop = '__Chain__',
     overriden,
     root = this,
@@ -10,9 +9,7 @@ var Chain = function(fn,opt_fn_or_bool){
   if(opt_fn_or_bool===true){
     fn.toString = function(){
       prototype['Chain'].fn = function(){
-        var arg = slice.call(arguments,0);
-        arg.unshift(this);
-        return fn.apply(root,arg);
+        return fn.apply(root,[].concat([this],slice.call(arguments,0)));
       }
       return prop;
     }
@@ -24,16 +21,15 @@ var Chain = function(fn,opt_fn_or_bool){
   }
   return fn;
 }
-Chain.version = .01;
+Chain.version = .2;
 
 overriden = Object.prototype[prop] || Object.prototype.isPrototypeOf || function(){};
 
 // If Chain versio => has been loaded, exit
-if(typeof overriden['Chain'] == 'funciton' && overriden['Chain'].version >= Chain.version){
+if(typeof overriden['Chain'] == 'function' && overriden['Chain'].version >= Chain.version){
   Chain = overriden['Chain'];
   exit = true;
 }
-
 // Server side support along with browser
 if(typeof module != 'undefined' && module.exports) {
   module.exports = Chain;
@@ -42,7 +38,7 @@ if(typeof module != 'undefined' && module.exports) {
 }
 // Exit if Chainy has been included before
 if(exit)return;
-console.log('loaded');
+
 function prototype(object){
   if(prototype['Chain'].fn){
     var ret = prototype['Chain'].fn.apply(this,arguments);
@@ -54,9 +50,9 @@ function prototype(object){
 
 try{
   Object.defineProperty(Object.prototype,prop,{value:prototype,writable:true,configurable:true});
-  for(var i in {})if(i===prototypeprop) throw 0;
+  for(var i in {})if(i===prop) throw 0;
 }catch(error){
-  // fallback for old IEs
+  // fallback for people supporting old IEs
   delete Object.prototype[prop];
   overriden = Object.prototype[prop = 'isPrototypeOf'];
   Object.prototype[prop] = prototype;
