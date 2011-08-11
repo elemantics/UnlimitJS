14bf4737d6d0b37b0ddbcab0ce2bfb4cf7e65006
Lime Desktop <limeblack7@gmai.com>
2011-08-11 02:09:28 -0400

    revert

--- unlimit.js
+++ unlimit.js
@@ -21,7 +21,7 @@ var Unlimit = function(fn,opt_fn_or_bool){
   }
   return fn;
 }
-Unlimit.version = 0.5.0;
+Unlimit.version = 0.5;
 
 overriden = Object.prototype[prop] || Object.prototype.isPrototypeOf || function(){};
 
